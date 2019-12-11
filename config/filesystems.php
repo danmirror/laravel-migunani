5a016955dc6c312a792bdc2a254c14e7ef6494d8
Danu andrean <danuandreanid@gmail.com>
2019-12-12 01:21:58 +0700

    doneeee

--- config/filesystems.php
+++ config/filesystems.php
@@ -49,10 +49,16 @@ return [
            
         ],
 
+        // 'public' => [
+        //     'driver' => 'local',
+        //     'root' => storage_path('app/public'),
+        //     'url' => env('APP_URL').'/storage',
+        //     'visibility' => 'public',
+        // ],
         'public' => [
             'driver' => 'local',
-            'root' => storage_path('app/public'),
-            'url' => env('APP_URL').'/storage',
+            'root' => public_path(),
+            'url' => env('APP_URL').'/public',
             'visibility' => 'public',
         ],
 
