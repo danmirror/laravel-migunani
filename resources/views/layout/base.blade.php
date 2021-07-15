d708ad0904ed1bcced00cff276bdc8c38fdfb743
danmirror <danuandreanid@gmail.com>
2021-07-15 16:05:51 +0700

    update footer

--- resources/views/layout/base.blade.php
+++ resources/views/layout/base.blade.php
@@ -70,7 +70,7 @@
                 <p>085225832895</p>
             </div>
         </div>
-        <div class="mx-3">
+        <div class="mx-4">
             <p class="mb-0 text-center"> &copy;  2019-<?php echo date("Y")?> | Migunani Architect. All right reserved </p>
         </div>
     </section>
